533e69c299e59c6b44fa046b86f7dbc4e03cd33a
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2024-01-03 14:04:18 +0100

    feat(core): added /json path

--- myApp.js
+++ myApp.js
@@ -3,11 +3,16 @@ let app = express();
 
 assets = __dirname + '/public'
 indexPage = __dirname + '/views/index.html'
+apiResponse = {"message": "Hello json"}
 
 app.use("/public", express.static(assets));
 
 app.get("/", (req, res) => {
     res.sendFile(indexPage)
+})
+
+app.get("/json", (req, res) => {
+    res.json(apiResponse)
 })
 
 module.exports = app

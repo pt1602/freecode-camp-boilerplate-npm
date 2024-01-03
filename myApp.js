42eee3ce09b87735769ec1cb99a0170095b82514
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2024-01-03 13:42:07 +0100

    feat(core): added assets middleware

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 assets = __dirname + '/public'
 indexPage = __dirname + '/views/index.html'
 
-app.use(express.static(assets));
+app.use("/public", express.static(assets));
 
 app.get("/", (req, res) => {
     res.sendFile(indexPage)

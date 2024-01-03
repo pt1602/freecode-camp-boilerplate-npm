f88bda1e45e6744c2ad62b0285748c9a2cdf9f11
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2024-01-03 09:57:55 +0100

    feat(core): return index.html on path /

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
 
+indexPage = __dirname + '/views/index.html'
+
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(indexPage);
 });
 
 module.exports = app;
